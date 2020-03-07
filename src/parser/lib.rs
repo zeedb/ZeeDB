8d0680f3fb5d849937875c5c30b22ae960b1be5a
George Fraser <george@fivetran.com>
2020-03-06 22:58:27 -0800

    Call analyze

--- src/parser/lib.rs
+++ src/parser/lib.rs
@@ -1,15 +1,30 @@
 use tokio::runtime::Runtime;
 use zetasql::local_service::zeta_sql_local_service_client::ZetaSqlLocalServiceClient;
-use zetasql::local_service::FormatSqlRequest;
+use zetasql::local_service::{AnalyzeRequest, analyze_request, analyze_response};
+use zetasql::{AnyResolvedStatementProto, ParseResumeLocationProto, SimpleCatalogProto};
 
 mod lib_tests;
 
-pub fn format(sql: String) -> String {
-    let mut runtime = Runtime::new().unwrap();
-    let mut client = runtime.block_on(ZetaSqlLocalServiceClient::connect("http://127.0.0.1:50051")).unwrap();
-    let request = tonic::Request::new(FormatSqlRequest {
-        sql: Some(String::from(sql)),
+pub fn analyze(sql: String, offset: i32, catalog: SimpleCatalogProto) -> Result<(i32, AnyResolvedStatementProto), String> {
+    let mut runtime = Runtime::new().expect("runtime failed to start");
+    let mut client = runtime.block_on(ZetaSqlLocalServiceClient::connect("http://127.0.0.1:50051")).expect("client failed to connect");
+    let request = tonic::Request::new(AnalyzeRequest {
+        simple_catalog: Some(catalog),
+        target: Some(analyze_request::Target::ParseResumeLocation(ParseResumeLocationProto{
+            input: Some(sql),
+            byte_position: Some(offset),
+            .. Default::default()
+        })),
+        .. Default::default()
     });
-    let response = runtime.block_on(client.format_sql(request)).unwrap().into_inner();
-    response.sql.unwrap()
+    let response = match runtime.block_on(client.analyze(request)) {
+        Ok(response) => response.into_inner(),
+        Err(status) => return Err(String::from(status.message())),
+    };
+    let offset = response.resume_byte_position.unwrap_or(-1);
+    let statement = match response.result.unwrap() {
+        analyze_response::Result::ResolvedStatement(statement) => statement,
+        _ => panic!("expression"),
+    };
+    Ok((offset, statement))
 }
