b102ba0190a7818e9fa8546090dfba875874fad9
George Fraser <george@fivetran.com>
2021-05-01 09:44:52 -0700

    Better log output

--- parser/server.rs
+++ parser/server.rs
@@ -1,30 +1,33 @@
 use std::process::Command;
 
 const SCRIPT: &str = r"
+set -e
+
 if [[ `docker ps --filter name=test-zetasql-server -q` ]]; then 
-    echo 'ZetaSQL server is already running'
     exit 0
 fi
 
 stopped=`docker ps --all --filter name=test-zetasql-server -q`
 
 if [[ $stopped ]]; then 
-    echo 'ZetaSQL server is stopped'
+    echo 'Restarting ZetaSQL server...' >&2
     docker restart $stopped
-    exit 0
+else 
+    echo 'Starting ZetaSQL server...' >&2
+    docker run \
+        --publish 127.0.0.1:50051:50051 \
+        --name test-zetasql-server \
+        --detach \
+        gcr.io/analog-delight-604/zetasql-server@sha256:62325de833178537b23ae83c8bb8dce6da51112491f3e7e9f101a262533b9df7
 fi
-
-docker run \
-    --publish 127.0.0.1:50051:50051 \
-    --name test-zetasql-server \
-    --detach \
-    gcr.io/analog-delight-604/zetasql-server@sha256:62325de833178537b23ae83c8bb8dce6da51112491f3e7e9f101a262533b9df7
 
 until nc -z 127.0.0.1 50051
 do
-    echo 'waiting for zetasql container...'
+
+    echo '...waiting for ZetaSQL server...' >&2
     sleep 0.5
 done
+echo '...ZetaSQL server has started' >&2
 ";
 
 /// Run the startup script for the local ZetaSQL server process.
