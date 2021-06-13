96ff5572a3c435fa9f4da0d02447c7acbf31238b
George Fraser <george@fivetran.com>
2021-06-13 12:02:45 -0700

    Dead code

--- kernel/lib.rs
+++ kernel/lib.rs
@@ -23,17 +23,3 @@ pub use crate::{
     any_array::*, array::*, array_like::*, bitmask::*, bool_array::*, data_type::*,
     exception::Exception, fixed_width::*, primitive_array::*, record_batch::*, string_array::*,
 };
-
-// fn map<'a>(array: A, f: F) -> AnyArray {
-//     todo!()
-// }
-
-// trait Unary<A, X> {
-//     fn apply(a: A) -> X;
-// }
-
-// trait Make<'a, OutElement, Out: Array<'a, Element = OutElement>> {
-//     fn make<'a>() -> ;
-// }
-
-// impl Make for StringArray {}
