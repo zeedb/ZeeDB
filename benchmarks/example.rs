cc604c910dfdbb81d857a745e14c38b01541508a
George Fraser <george@fivetran.com>
2021-07-04 09:02:23 -0700

    Fix names

--- benchmarks/example.rs
+++ benchmarks/example.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use e2e_tests::TestRunner;
 
 fn bench(c: &mut Criterion) {
-    c.bench_function("insert into test values (i)", |b| {
+    c.bench_function("insert into test values (@i)", |b| {
         let mut t = TestRunner::default();
         t.run("create table test (i int64)", vec![]);
         let mut i = 0;
@@ -17,7 +17,7 @@ fn bench(c: &mut Criterion) {
         });
     });
 
-    c.bench_function("insert into table_with_index values (i)", |b| {
+    c.bench_function("insert into table_with_index values (@i)", |b| {
         let mut t = TestRunner::default();
         t.run("create table table_with_index (i int64)", vec![]);
         t.run("create index index_i on table_with_index (i)", vec![]);
@@ -33,7 +33,7 @@ fn bench(c: &mut Criterion) {
     });
 
     c.bench_function(
-        "update table_with_index set indexed_column = i where indexed_column = i - 1",
+        "update table_with_index set indexed_column = @i where indexed_column = @i - 1",
         |b| {
             let mut t = TestRunner::default();
             t.run(
