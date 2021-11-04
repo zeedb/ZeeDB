a2e2e91048c5800a96317e00d4393d5094bd37d8
George Fraser <george@fivetran.com>
2021-11-04 11:38:29 +0000

    More aggregate tests

--- sqllogictest/tests/aggregate.rs
+++ sqllogictest/tests/aggregate.rs
@@ -4,5 +4,6 @@ use sqllogictest::runner::test;
 fn test_aggregates() {
     rpc::runtime().block_on(test(vec![
         "./tests/duckdb/aggregate/aggregates/test_aggr_string.test",
+        "./tests/duckdb/aggregate/aggregates/test_aggregate_types_scalar.test",
     ]));
 }
