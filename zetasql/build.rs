19e643b4ca716cbf5cc22a09ca6c4ae6a4d00195
George Fraser <george.w.fraser@gmail.com>
2021-11-22 10:46:37 -0800

    Bootstrap build correctly

--- zetasql/build.rs
+++ zetasql/build.rs
@@ -1,5 +1,9 @@
 fn main() {
     println!("cargo:rerun-if-changed={}", "./protos");
+    // Create these files if they don't already exist.
+    touch("./local_service.rs").unwrap();
+    touch("./zetasql.rs").unwrap();
+    // Generate protobuf rust implementations from definitions.
     tonic_build::configure()
         .build_server(false)
         .format(option_env!("NO_RUSTFMT") != Some("1"))
@@ -9,6 +13,16 @@ fn main() {
             &["./protos"],
         )
         .unwrap();
+    // Rename bad file names.
     std::fs::rename("./zetasql.local_service.rs", "./local_service.rs").unwrap();
+    // This file is empty.
     std::fs::remove_file("./google.protobuf.rs").unwrap();
+}
+
+fn touch(path: impl AsRef<std::path::Path>) -> std::io::Result<()> {
+    std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(path)
+        .map(|_| ())
 }
