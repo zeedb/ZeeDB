b8d35665dd33de37888985afb61bb3635ae0d6cb
George Fraser <george.w.fraser@gmail.com>
2021-11-22 11:08:31 -0800

    More aggregate tests

--- sqllogictest/tests/aggregate.rs
+++ sqllogictest/tests/aggregate.rs
@@ -13,5 +13,8 @@ fn test_aggregates() {
         "./tests/duckdb/aggregate/aggregates/test_distinct_aggr.test",
         "./tests/duckdb/aggregate/aggregates/test_empty_aggregate.test",
         "./tests/duckdb/aggregate/aggregates/test_first_noninlined.test",
+        "./tests/duckdb/aggregate/aggregates/test_incorrect_aggregate.test",
+        "./tests/duckdb/aggregate/aggregates/test_scalar_aggr.test",
+        "./tests/duckdb/aggregate/aggregates/test_sum.test",
     ]));
 }
